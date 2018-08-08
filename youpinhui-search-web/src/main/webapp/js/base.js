9c4394038fa0e3a5d87b914a61e7014176b5bc8f
leon846666 <leon846666@gmail.com>
2018-08-08 10:45:28 -0400

    add filter,use $sce component, call function trustAsHtml

--- youpinhui-search-web/src/main/webapp/js/base.js
+++ youpinhui-search-web/src/main/webapp/js/base.js
@@ -1,1 +1,9 @@
 var app = angular.module("youpinhui",[]);
+
+//filter
+app.filter('trustHtml',['$sce',function($sce){
+
+    return function(data){// send data that needs to be filtered
+        return $sce.trustAsHtml(data);  //return the data has been filtered
+    }
+}])
