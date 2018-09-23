ff35829b4a29d271775eedbf5d2529ce2814406f
leon846666 <leon846666@gmail.com>
2018-09-22 22:30:49 -0400

    completed the structure of cart module ,add cookie util.

---
+++ youpinhui-cart-web/src/main/java/com/youpinhui/user/service/UserDetailServiceImpl.java
@@ -0,0 +1,26 @@
+package com.youpinhui.user.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		
+		
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+		return new User(username,"",authorities);
+	}
+
+	
+}
