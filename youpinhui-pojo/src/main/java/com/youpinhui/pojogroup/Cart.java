faac5cb2cae90ce825d650bf240b537255396f83
leon846666 <leon846666@gmail.com>
2018-09-23 14:20:13 -0400

    create cart object ,implements serializeble interface

---
+++ youpinhui-pojo/src/main/java/com/youpinhui/pojogroup/Cart.java
@@ -0,0 +1,47 @@
+package com.youpinhui.pojogroup;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+import com.youpinhui.pojo.TbOrderItem;
+
+
+
+/**
+ *  
+ *  Object Cart
+ *  
+ */
+
+public class Cart implements Serializable {
+
+	private String sellerId;
+	private String sellName;
+	
+	private List<TbOrderItem> orderItemList;//ordered Items list
+
+	public String getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(String sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public String getSellName() {
+		return sellName;
+	}
+
+	public void setSellName(String sellName) {
+		this.sellName = sellName;
+	}
+
+	public List<TbOrderItem> getOrderItemList() {
+		return orderItemList;
+	}
+
+	public void setOrderItemList(List<TbOrderItem> orderItemList) {
+		this.orderItemList = orderItemList;
+	}
+}
